9edf40d6ed0ca3a998e4d3f550927e19b0813503
“Melvin <melvintorresg@gmail.com>
2024-09-13 04:07:05 -0600

    Ajuste

--- Api/Usuarios/obj/Release/net8.0/Usuarios.AssemblyInfo.cs
+++ Api/Usuarios/obj/Release/net8.0/Usuarios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Usuarios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb24356c62f6b29a32d69b30d57194cd896fad0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b1e7f366501b5aa2fe9e48f3cb1407c9c4a5c5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Usuarios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Usuarios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
